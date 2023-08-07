98f3154ae1da15a2c0717e7aa13848e865fe12f2
ThatgirlAm <atm_diop@icloud.com>
2023-08-07 19:06:06 +0000

    Try 1

--- deleting_"liked".py
+++ deleting_"liked".py
@@ -1,34 +1,48 @@
 #python automation project 1 : effacer les notifications contenant le mot "liked".
 
-#ici l'on se connectera avec l'appareil à travers l'interface ADB téléchargée au préalable
+#-------------------------------Explanations----------------------------------------
+#here's how we'll proceed : we'll access the phone through ADB and read notifications, 
+#when "liked" is encountered, the whole line (which is the whole notification is deleted)
+#we'll use an infinite loop + the sleep feature of the time library to add an interval
+#which will result in running the script every two hours (every 7200 seconds)
 
-#import de la bibliothèque python subprocess qui nous permet de fouiller l'appareil avec l'application ADB (Android Debug Bridge)
+#-------------------------------Coding----------------------------------------------
 
-import subprocess
+#importing subprocess to connect through ADB (Android Debug Bridge) 
+import subprocess, time
 
-#Etape 1:  Connexion à l'appareil avec ADB
-def connexion():
-    # Exécute la commande adb pour connecter l'appareil
+#Step 1 : connecting to the phone with adb 
+def connecting():
+    #Executing the connecting command to connect to the phone through ADB
     subprocess.call(["adb", "devices"])
 
 
-#Etape 2: Fonction pour supprimer les notifications contenant le mot "liked"
-def suppression_notifications_specifiques():
+#Estep 2 : deleting the "liked" notifications every 2 hours 
+def deleting_notifications():
 	
-    # Génération de la liste des notifications
-    notifications = subprocess.check_output(["adb", "shell", "dumpsys", "notification", "--noredact"]).decode("utf-8")
-    
-    # Itération sur le nombre de lignes 
-    for line in notifications.split("\n"):
-    	
-        # Test condition
-        if "liked" in line:
-        	
-            # Exécute la commande adb pour supprimer la notification 
-            notification_id = line.split(" ")[1].split(":")[1]
-            subprocess.call(["adb", "shell", "service", "call", "notification", notification_id])
+   #generating notifications list
+   #infinite loop 
+            notifications = subprocess.check_output(["adb", "shell", "dumpsys", "notification", "--noredact"]).decode("utf-8")
+            
+                #lines itérations (notifications)
+                for line in notifications.split("\n"):
+                    
+                    #condition testing 
+                    if "liked" in line:
+                        
+                        #deleting the notification through ADB 
+                        notification_id = line.split(" ")[1].split(":")[1]
+                        subprocess.call(["adb", "shell", "service", "call", "notification", notification_id])
 
 
-#Appel des fonctions
-connexion()
-suppression_notifications_specifiques()
+#-------------------------------Running the scripts-----------------------------------------------------------
+#avoiding hiccups   
+   while (True):
+        #avoiding hiccups 
+        try : 
+            connecting()
+            deleting_notifications()
+            time.sleep(7200)
+        except:
+        print ("Script error or human intervention")
+
